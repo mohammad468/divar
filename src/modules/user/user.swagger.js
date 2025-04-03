5d0e9bf3647e630554e1ac5c3332a4564c7d4bb0
mohammad468 <mohammad.mohseni468@gmail.com>
2025-04-03 15:18:42 +0330

    complete swagger in user model

--- src/modules/user/user.swagger.js
+++ src/modules/user/user.swagger.js
@@ -7,11 +7,96 @@
 
 /**
  * @swagger
+ * components:
+ *   schemas:
+ *     CreateUser:
+ *       type: object
+ *       required:
+ *         - mobile
+ *       properties:
+ *         fullName:
+ *           type: string
+ *         mobile:
+ *           type: string
+ */
+
+/**
+ * @swagger
  * /user/whoami:
  *   get:
  *     summary: Get User Profile
  *     tags:
  *       - User
+ *     parameters:
+ *       - in: header
+ *         name: Authorization
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Bearer token (e.g., "Bearer your_token_here")
+ *     responses:
+ *       200:
+ *         description: Success
+ */
+
+/**
+ * @swagger
+ * /user/:
+ *   get:
+ *     summary: Get All Users
+ *     tags:
+ *       - User
+ *     parameters:
+ *       - in: header
+ *         name: Authorization
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Bearer token (e.g., "Bearer your_token_here")
+ *     responses:
+ *       200:
+ *         description: Success
+ */
+
+/**
+ * @swagger
+ * /user/:
+ *   post:
+ *     summary: Create user
+ *     tags:
+ *       - User
+ *     parameters:
+ *       - in: header
+ *         name: Authorization
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Bearer token (e.g., "Bearer your_token_here")
+ *     responses:
+ *       200:
+ *         description: Success
+ */
+
+/**
+ * @swagger
+ * /user/{id}:
+ *   delete:
+ *     summary: Delete  User
+ *     tags:
+ *       - User
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID of the category to delete
+ *       - in: header
+ *         name: Authorization
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Bearer token (e.g., "Bearer your_token_here")
  *     responses:
  *       200:
  *         description: Success
